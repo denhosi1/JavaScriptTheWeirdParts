ff8b65f284d7e336c0017f7d273a0ff9a0a80734
pog mahone <pog mahone>
2016-07-12 20:13:20 +1200

    Modified index, app.js and added powerpoint

--- app.js
+++ app.js
@@ -1,2 +1,206 @@
-console.log("Test This");
-console.log("Then test that");
+ /*****     THIS KEYWORD     *****/
+
+/* Example One */
+console.log(window);
+console.log(this);
+
+/* Example Two */
+ var firstname = "James";
+ var lastname = "Bond";
+
+function showfullname () {
+	console.log (this.firstname + " " + this.lastname);
+}
+
+var person = {
+	firstname:"Jason",
+	lastname:"Bourne",
+	showfullname:function() {
+		console.log (this.firstname + " " + this.lastname);
+	}
+}
+
+showfullname();
+
+window.showfullname();
+
+this.showfullname();
+
+person.showfullname();
+
+this.person.showfullname();
+
+/* Example Three */
+var user = {
+	data:[
+		{name:"T. Woods", age:37},
+		{name:"P. Mickelson", age:43}
+	],
+	clickHandler:function (event) {
+		var randomNum = ((Math.random () * 2 | 0) + 1) - 1;
+		console.log (this.data[randomNum].name + " " + this.data[randomNum].age);
+	}
+}
+
+document.getElementById("button").addEventListener("click", user.clickHandler);
+//What will the output be here?
+
+/******      EQUALITY      ******/
+
+/* Example One */
+console.log(1 == 1);
+console.log(1 == '1');
+console.log(1 == 2);
+console.log('' == false);
+console.log([] == false);
+console.log(null == undefined);
+
+/* Example Two */
+console.log(1 === 1);
+console.log(1 === '1');
+console.log(1 === parseInt('1'));
+console.log([] === false);
+console.log(null === undefined);
+
+/******      THE SCOPE CHAIN      ******/
+
+/* Example One */
+function b() {
+	console.log(myVar);
+	//Will this print 'Undefined', '1' or '2'?
+}
+
+function a() {
+	var myVar = 2;
+	b();
+}
+
+var myVar = 1;
+a();
+
+/* Example Two */
+function c() {
+
+	function d() {
+		console.log(myNewVar);
+		//Will this print 'Undefined', '1' or '2'?
+	}
+
+	var myNewVar = 2;
+	d();
+}
+
+var myNewVar = 1;
+c();
+
+
+/******      AUTOMATIC SEMICOLON INSERTION      ******/
+
+/* Example One */
+function doStuff() {
+	return
+	{
+		status: true
+	}
+}
+	
+function doStuffAgain() {
+	return {
+		status: true
+	}
+}
+	
+console.log(doStuff());
+console.log(doStuffAgain());
+
+/******      EXECUTION CONTEXT - CREATION AND HOISTING      ******/
+
+/* Example One */
+var nerf = "Hello Nerf!";
+
+function NerfThis() {
+	console.log("Called NerfThis");
+}
+
+NerfThis();
+console.log(nerf);
+
+/* Example Two */
+DontNerfThis();
+console.log(newNerf);
+
+var newNerf = "Hello World!";
+
+function DontNerfThis() {
+	console.log("Called DontNerfThis");
+}
+
+/******      FIRST CLASS FUNTIONS      ******/
+function ThisIsAnObject() {
+	console.log("I am an object");
+}
+
+ThisIsAnObject.NickName = "foobar";
+
+ThisIsAnObject();
+console.log(ThisIsAnObject.NickName);
+
+var anonymousGreet = function() {
+	console.log("This");
+}
+
+anonymousGreet.test = "test";
+
+anonymousGreet();
+console.log(anonymousGreet.test);
+
+/******      FUNCTIONAL PROGRAMMING      ******/
+
+/* Example One */
+var arr1 = [1,2,3];
+console.log(arr1);
+
+var arr2 = [];
+for (var i = 0; i < arr1.length; i++) {
+	arr2.push(arr1[i] * 2);
+};
+
+console.log(arr2);
+
+/* Example Two */
+function mapForEach(arr, fn) {
+	var newArr = [];
+
+	for (var i = 0; i < arr.length; i++) {
+		newArr.push(
+			fn(arr[i])
+		);
+	};
+
+	return newArr;
+}
+
+var arr3 = mapForEach(arr1, function(item){
+	return item * 3;
+})
+
+console.log(arr3);
+
+var arr4 = mapForEach(arr1, function(item){
+	return item > 2;
+})
+
+console.log(arr4);
+
+/******      IMMEDIATELY INVOKED FUNCTION EXPRESSIONS      ******/
+var greeting = function() {
+	return "Nerf this";
+}
+
+console.log(greeting);
+
+var newGreeting = function() {
+	return "Nerf That";
+}()
+
+console.log(newGreeting);
